c42d62d384cf1c89cc289e79ebe5061be460fbcf
sezium <ssezium@proton.me>
2026-08-24 17:23:49 +0200

    tmux fixes

--- home-manager/modules/cli/tmux.nix
+++ home-manager/modules/cli/tmux.nix
@@ -5,26 +5,19 @@
     baseIndex = 1;
     mouse = true;
     escapeTime = 0;
-
     historyLimit = 1000;
     keyMode = "vi";
     terminal = "xterm-256color";
-
     extraConfig = ''
       unbind C-b
-
-      # default shell 
+      # default shell
       set -g default-command ${pkgs.zsh}/bin/zsh
-
-
       # required by yazi to work properly
       set -g allow-passthrough on
       set -ga update-environment TERM
       set -ga update-environment TERM_PROGRAM
-
       set -s escape-time 0
       set -as terminal-features ",kitty*:RGB"
-
       set -g @resurrect-dir '~/.tmux/resurrect'
       set -g @continuum-restore 'on'
 
@@ -41,9 +34,9 @@
       bind -n M-k resize-pane -U 3
       bind -n M-j resize-pane -D 3
 
-      bind -n M-H split-window -v -c "#{pane_current_path}"
-      bind -n M-V split-window -h -c "#{pane_current_path}"
-
+      # split (spostati da M-H/M-V perche' ora usati per lo swap)
+      bind -n M-s split-window -v -c "#{pane_current_path}"
+      bind -n M-a split-window -h -c "#{pane_current_path}"
       bind -n M-t new-window
 
       bind-key -T copy-mode-vi 'v' send -X begin-selection
@@ -51,19 +44,28 @@
 
       bind -n M-q kill-pane
       bind -n M-Q kill-window
-      bind -n M-s kill-session
-      '';
+      # NOTA: kill-session era su M-s, ora libero perche' M-s e' split-window.
+      # Scegli un altro tasto, es. M-Q maiuscolo e' gia' preso da kill-window.
+      # bind -n M-? kill-session
 
+      # Alt+Shift+hjkl: scambia il pane attivo con quello adiacente nella direzione
+      bind -n M-H swap-pane -t '{left-of}'
+      bind -n M-J swap-pane -t '{down-of}'
+      bind -n M-K swap-pane -t '{up-of}'
+      bind -n M-L swap-pane -t '{right-of}'
+
+      # Ctrl+hjkl: gestito automaticamente da tmuxPlugins.vim-tmux-navigator
+      # (non ridefinire qui C-h/j/k/l, altrimenti si sovrascrivono i bind del plugin,
+      # che ha gia' una regex piu' completa: supporta fzf, nvim con wrapper, ecc.)
+      '';
     plugins = with pkgs;
     [
       tmuxPlugins.vim-tmux-navigator
       tmuxPlugins.yank # Copy to system clipboard
-      tmuxPlugins.gruvbox # -- 
+      tmuxPlugins.gruvbox # --
       tmuxPlugins.better-mouse-mode # Mouse support
       tmuxPlugins.tmux-fzf
       tmuxPlugins.copycat
-
-
       tmuxPlugins.resurrect
       tmuxPlugins.continuum
     ];
